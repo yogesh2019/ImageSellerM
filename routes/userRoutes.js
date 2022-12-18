0bb6334d98877b678027a8d0dbbb53314f221703
yogesh2019 <yogesh.khichi@gmail.com>
2022-12-19 00:19:54 +0530

    bcrypt.compare(password, existingUser.password), adding signin api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { validateName, validateEmail, validatePassword } = require('../utils/validators');
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
-const { PassThrough } = require('stream');
 
 router.post('/signup', async (req, res) => {
     try {
@@ -34,6 +33,41 @@ router.post('/signup', async (req, res) => {
     } catch (error) {
         console.log(error);
         return res.status(500).send(error);
+    }
+});
+
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (email.length === 0) {
+            return res.status(400).json({
+                err: "Please provide Email"
+            })
+        }
+        if (password.length === 0) {
+            return res.status(400).json({
+                err: "Please provide password"
+            })
+        }
+        const existingUser = await User.findOne({ where: { email } });
+        if(!existingUser){
+            return res.status(403).json({
+                err: "User not found"
+            });
+        }
+        const passwordMatched = await bcrypt.compare(password, existingUser.password);
+        if(!passwordMatched){
+            return res.status(403).json({
+                err: "email or password mismatch"
+            })
+        }
+        return res.status(200).json({
+                success: `Welcome ${existingUser.email}`
+            });
+
+        
+    } catch (error) {
+            return res.status(500).send(error);
     }
 });
 module.exports = router;
