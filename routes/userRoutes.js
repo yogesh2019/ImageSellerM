c87e9c6d52a381809d6b676f09f580b81ab6c61f
yogesh2019 <yogesh.khichi@gmail.com>
2022-12-18 23:41:34 +0530

    keep return res.json in same func,name validation and passwrd hash,using validators with neg

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,17 +2,34 @@ const express = require('express');
 const router = express.Router();
 const { validateName, validateEmail, validatePassword } = require('../utils/validators');
 const User = require('../models/userModels');
+const bcrypt = require('bcrypt');
+const { PassThrough } = require('stream');
 
 router.post('/signup', async (req, res) => {
     try {
-        const { email } = req.body;
+        const { name, email, password, isSeller } = req.body;
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
             return res.status(403).json({ err: "User already exists" });
         }
-        console.log(existingUser);
-        return res.json({
-            ok : 'ok'
+        if (!validateName(name)) {
+            console.log(name);
+            return res.status(400).json({ err: "Invalid Name" });
+        }
+        if (!validateEmail(email)) {
+            return res.status(400).json({ err: "Invalid Email" });
+        }
+        if (!validatePassword(password)) {
+            return res.status(400).json({ err: "Invalid Password" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = {
+            name, email, password: hashedPassword, isSeller
+        }
+        const createdUser = await User.create(user);
+        return res.status(200).json({
+            message: `Welcome ${createdUser.name}`,
         });
     } catch (error) {
         console.log(error);
